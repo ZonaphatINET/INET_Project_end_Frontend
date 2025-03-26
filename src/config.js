24b9100c8136d1c4992dbd0687472fae71415266
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2025-03-27 03:00:34 +0700

    update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
-const API_URL = process.env.NODE_ENV === 'production' 
-              ? 'http://185.84.161.66:5000'
-              : 'http://localhost:5000';
+const API_URL = process.env.NODE_ENV === 'production'
+  ? 'https://185.84.161.178'
+  : 'http://localhost:5000';
 
-export default API_URL; 
+export default API_URL;
