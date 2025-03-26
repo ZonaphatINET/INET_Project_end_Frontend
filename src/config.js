1b2f93533135747c8f503f1381b813aeb19c84dc
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2025-03-27 01:56:45 +0700

    update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const API_URL = process.env.NODE_ENV === 'production' 
-              ? 'http://185.84.161.66:5000'  // เปลี่ยนเป็น HTTPS
+              ? 'http://185.84.161.66:5000'
               : 'http://localhost:5000';
 
 export default API_URL; 
