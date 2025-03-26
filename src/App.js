6f5a2fd7bb0e9a36c0201ccf6af26edd47ef69ff
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2025-03-26 19:46:37 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 // Import components
 import Login from "./components/Login";
